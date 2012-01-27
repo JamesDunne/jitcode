e402f104b94a34ce832dd64bde09da71fcfa38f8
James Dunne <james.jdunne@gmail.com>
2012-01-26 19:20:14 -0600

    Added 0xFFFF stop opcode and moved function compiler to function.

--- main.c
+++ main.c
@@ -20,26 +20,40 @@ const jit_bin_op_t code[] = {
     { JIT_GETARG,       SPEC(TREG, IMM, NO),    C_IR( 0),       (jit_value)(0),     0,              0 },
     { JIT_ADD | IMM,    SPEC(TREG, REG, IMM),   C_IR( 1),       C_IR( 0),           (jit_value)1,   0 },
     { JIT_RET | REG,    SPEC(REG, NO, NO),      C_IR( 1),       0,                  0,              0 },
+    { 0xFFFF,           0,                      0,              0,                  0,              0 },
 };
 const int code_length = sizeof(code) / sizeof(code[0]);
 
 typedef int64_t (*func_t)(int64_t);
 
+void *jit_bin_load_func(struct jit *ctx, const jit_bin_op_t opcodes[], int *op)
+{
+    // Create the function prolog:
+    void *func;
+    jit_prolog(ctx, &func);
+
+    // Enter in the function opcodes from the data:
+    for (; ; (*op)++)
+    {
+        int i = *op;
+        if (opcodes[i].code == 0xFFFF || opcodes[i].code == JIT_CODESTART)
+            break;
+        jit_add_op(ctx, opcodes[i].code, opcodes[i].spec, opcodes[i].arg1, opcodes[i].arg2, opcodes[i].arg3, opcodes[i].arg_size);
+    }
+
+    // Compile the function and copy the native machine code to `func`:
+    jit_generate_code(ctx);
+
+    return func;
+}
+
 int main(void)
 {
     struct jit *ctx = jit_init();
 
-    // Create the function prolog:
-    func_t func;
-    jit_prolog(ctx, &func);
-
-    // Enter in the function opcodes from the data:
-    for (int i = 0; i < code_length; ++i)
-    {
-        jit_add_op(ctx, code[i].code, code[i].spec, code[i].arg1, code[i].arg2, code[i].arg3, code[i].arg_size);
-    }
-
-    jit_generate_code(ctx);
+    int op = 0;
+    func_t func = (func_t) jit_bin_load_func(ctx, code, &op);
+    printf("%d opcodes\n", op);
 
     int64_t b = func(1);
     printf("%lu\n", b);
